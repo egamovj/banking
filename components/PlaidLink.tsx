72151c95cb61ce8d6ab8eb3cb694895015ab692e
egamovj <egamovj90@gmail.com>
2024-07-13 09:43:55 +0500

    plaid

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,9 +1,16 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { Button } from "./ui/button";
-import { PlaidLinkOptions } from "react-plaid-link";
+import {
+  PlaidLinkOnSuccess,
+  PlaidLinkOptions,
+  usePlaidLink,
+} from "react-plaid-link";
 import { StyledString } from "next/dist/build/swc";
+import { useRouter } from "next/navigation";
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+  const router = useRouter();
+
   const [token, setToken] = useState("");
 
   useEffect(() => {
@@ -15,14 +22,14 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
     getLinkToken();
   }, []);
 
-  const onSuccess = useCallback(
-    async (public_token: StyledString) => {
+  const onSuccess = useCallback<PlaidLinkOnSuccess>(
+    async (public_token: string) => {
       //   await exchangePublicToken({
       //     publicToken: public_token,
       //     user,
       //   });
 
-      
+      router.push("/");
     },
     [user]
   );
@@ -32,10 +39,18 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
     onSuccess,
   };
 
+  const { open, ready } = usePlaidLink(config);
+
   return (
     <div>
       {variant === "primary" ? (
-        <Button className="plaidlink-primary">Connect bank</Button>
+        <Button
+          onClick={() => open()}
+          disabled={!ready}
+          className="plaidlink-primary"
+        >
+          Connect bank
+        </Button>
       ) : variant === "ghost" ? (
         <Button>Connect bank</Button>
       ) : (
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,8 @@ import { cookies } from "next/headers";
 import { createAdminClient, createSessionClient } from "../server/appwrite";
 import { ID } from "node-appwrite";
 import { parseStringify } from "../utils";
+import { CountryCode, Products } from "plaid";
+import { plaidClient } from "../plaid";
 
 export const signIn = async ({ email, password }: signInProps) => {
   try {
@@ -66,5 +68,25 @@ export const logoutAccount = async () => {
     await account.deleteSession("current");
   } catch (error) {
     return null;
+  }
+};
+
+export const createLinkToken = async (user: User) => {
+  try {
+    const tokenParams = {
+      user: {
+        client_user_id: user.$id,
+      },
+      client_name: user.name,
+      products: ["auth"] as Products[],
+      language: "en",
+      country_codes: ["US"] as CountryCode[],
+    };
+
+    const response = await plaidClient.linkTokenCreate(tokenParams);
+
+    return parseStringify({ linkToken: response.data.link_token });
+  } catch (error) {
+    console.log(error);
   }
 };
